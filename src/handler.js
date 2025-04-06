75e8db2549abf36248c4b016076fe9ad3358b1a9
rizalsuryana <rizal.suryn@gmail.com>
2025-04-07 04:24:42 +0700

    on handler.js add  edit book and delete book

--- src/handler.js
+++ src/handler.js
@@ -125,8 +125,8 @@ const getAllBooksHandler = (request, h) => {
 
 };
 
-// Menampilkan detail buku
 
+// Menampilkan detail buku
 const getBookByIdHandler = (request, h) => {
   const { bookId } = request.params;
   const book = books.find((b)=> b.id === bookId);
@@ -144,4 +144,87 @@ const getBookByIdHandler = (request, h) => {
       book
     }
   }).code(200);
+};
+
+
+// Mengubah data buku
+const editBookByIdHandler = (request, h) => {
+  const { bookId } = request.params;
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading
+  } = request.payload;
+
+  if (!name) {
+    return h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Mohon isi nama buku',
+    }).code(400);
+  }
+
+  if (readPage > pageCount) {
+    return h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount'
+    }).code(400);
+  }
+
+  const index = books.findIndex((book) => book.id === bookId);
+
+  if (index === -1) {
+    return h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Id tidak ditemukan'
+    }).code(404);
+  }
+
+  const updatedAt = new Date().toISOString();
+  const finished = pageCount === readPage;
+
+  books[index] = {
+    ...books[index],
+    name, year, author, summary, publisher,
+    pageCount, readPage, reading,
+    finished, updatedAt,
+  };
+
+  return h.response({
+    status: 'success',
+    message: 'Buku berhasil diperbarui'
+  }).code(200);
+};
+
+
+// Menghapus buku
+const deleteBookByIdHandler = (request, h) => {
+  const { bookId } = request.params;
+  const index = books.findIndex((book) => book.id === bookId);
+
+  if (index === -1) {
+    return h.response({
+      status: 'fail',
+      message: 'Buku gagal dihapus. Id tidak ditemukan',
+    }).code(404);
+  }
+
+  books.splice(index, 1);
+
+  return h.response({
+    status: 'success',
+    message: 'Buku berhasil dihapus',
+  }).code(200);
+};
+
+module.exports = {
+  addBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler
 };
